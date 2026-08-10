e49ab47c1cb751679145fb68fd33bc0468c6733e
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 15:55:19 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/io/github/easy4j/opencli/spring/boot/OpenCliCliStartupChecker.java
+++ src/main/java/io/github/easy4j/opencli/spring/boot/OpenCliCliStartupChecker.java
@@ -14,7 +14,7 @@ import org.springframework.core.env.Environment;
 /**
  * 应用启动时探测本机 {@code opencli} 是否可用（远程 Agent 模式自动跳过）。
  *
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  * @since 1.0.0
  */
 @Slf4j
