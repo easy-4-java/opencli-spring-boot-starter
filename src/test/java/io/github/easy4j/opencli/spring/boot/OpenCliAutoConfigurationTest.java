e90469c959d0d7a6adeb54b2a7bb5ef08b7cbede
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:45:30 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/test/java/io/github/easy4j/opencli/spring/boot/OpenCliAutoConfigurationTest.java
+++ src/test/java/io/github/easy4j/opencli/spring/boot/OpenCliAutoConfigurationTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 /**
  * 校验 Starter 能注册属性、执行器与 {@link OpenCliClient}。
  *
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  * @since 1.0.0
  */
 @SpringBootTest(
